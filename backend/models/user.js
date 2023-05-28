6b6db9d3433209c73d8016b0dc5b7bf32d0d0dad
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-05-28 21:16:42 +0100

    change user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  // userName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -11,28 +10,19 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://i.imgur.com/1Q9ZQ9u.png',
   },
-  interests: { type: Array, required: true },
-  birthday: { type: Date, required: true },
-  gender: { type: String, required: true },
-  chats: [],
 });
 
 //hashes and salts a password
-
-userSchema.pre('save', function (next) {
-  const user = this;
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      next();
-    });
-  });
+userSchema.pre('save', async function (next) {
+  try {
+    const user = this;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('User', userSchema);
