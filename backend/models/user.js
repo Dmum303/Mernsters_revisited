53e682affb92d2000763840a0ef16bca14848283
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-05-30 14:33:50 +0100

    recofig db

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,13 +2,14 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: { type: String, required: true, maxlength: 15 },
+  lastName: { type: String, required: true, maxlength: 15 },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   profilePic: {
     type: String,
-    default: 'https://i.imgur.com/1Q9ZQ9u.png',
+    default:
+      'https://plus.unsplash.com/premium_photo-1678523786247-db3e2ac05e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
   },
 });
 
