0af8c14cb69988ddb7a0a71f8b37f425e9de20de
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-05-30 15:36:56 +0100

    updating mogodb helper in spec

--- backend/spec/mongodb_helper.js
+++ backend/spec/mongodb_helper.js
@@ -1,20 +1,12 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-beforeAll(function (done) {
-  mongoose.connect('mongodb://0.0.0.0/mernsters_test', {
+beforeAll(async () => {
+  await mongoose.connect('mongodb://0.0.0.0/mernsters_test', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  db.on('open', function () {
-    done();
-  });
 });
 
-afterAll(function (done) {
-  mongoose.connection.close(true, function () {
-    done();
-  });
+afterAll(async () => {
+  await mongoose.connection.close();
 });
